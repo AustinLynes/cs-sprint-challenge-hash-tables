629fc7a1735a8f9729b46e2c03dccd0f6c4ed982
AustinLynes <austinlynes94@gmail.com>
2020-07-17 20:20:09 -0700

    wasnt able to make it... :/

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -1,7 +1,48 @@
-def get_indices_of_item_weights(weights, length, limit):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+# Given a package with a weight limit `limit` and a list `weights` of item
+# weights, implement a function `get_indices_of_item_weights` that finds
+# two items whose sum of weights equals the weight limit `limit`. Your
+# function will return a tuple of integers that has the following form:
+# ```
+# (zero, one)
+# ```
 
-    return None
+# where each element represents the item weights of the two packages.
+# _**The higher valued index should be placed in the `zeroth` index and
+# the smaller index should be placed in the `first` index.**_ If such a
+# pair doesn’t exist for the given inputs, your function should return
+
+# Example:
+# ```
+# input: weights = [ 4, 6, 10, 15, 16 ], length = 5, limit = 21
+# output: [ 3, 1 ]  # since these are the indices of weights 15 and 6 whose sum equals 21
+# ```
+
+
+"""
+YOUR CODE HERE
+"""
+def get_indices_of_item_weights(weights, length, limit):
+    # if you give me invalid input then im gonna give you nothing in return
+    if length <= 1:
+        return None
+    
+    _table = {}
+    _indices = []
+    cur = 0;
+
+    while cur < length:
+
+        cur += 1 
+        
+    for k, v in _table.items():
+    
+        if k < length - 1:
+            if _table[k] + _table[k + 1] == limit:
+                _indices.append(_table[k])
+                _indices.append(_table[k + 1])
+
+    return _indices
+
+weights_2 = [4, 4]
+print(get_indices_of_item_weights(weights_2, 2, 8))
+#  [ 1, 0 ]
--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,33 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
 
+    route = []
+    # look at each ticket
+    c = 1
+    for ticket in tickets:
+        # if the 
+     
+        
+        # if ticket.source == "NONE":
+        #     print('found one')
+        #     route.append(ticket.destination)
+
+        if ticket.destination == tickets[c].source:
+            route.append(ticket.destination)
+
+
+        if ticket.destination == "NONE" and len(route) is len(tickets) - 1:
+            route.append(ticket.destination)
+        
+        if c + 1 < len(tickets):
+            c += 1
+
+    
+        if ticket == tickets[len(tickets)-1] and len(route) is not len(tickets):
+                ticket = tickets[0]
+                c = 1
+
+        
+    print(route)
     return route
